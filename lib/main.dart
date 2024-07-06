e4ee598acfd03249497ea857e7b2aa7aeb56ef28
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-07-06 13:20:57 +0200

    Added icons and bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,50 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        body: const Center(
           child: Text('Hello World!'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: const Color.fromARGB(255, 29, 31, 36),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextButton(
+                onPressed: () {},
+                child: Column(children: [
+                  Image.asset('assets/icons/bottom-nav/home.png',
+                      width: 24.0, height: 24.0),
+                  const Text('Home'),
+                ]),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Column(children: [
+                  Image.asset('assets/icons/bottom-nav/group-events.png',
+                      width: 24.0, height: 24.0),
+                  const Text('Mates'),
+                ]),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Column(children: [
+                  Image.asset('assets/icons/bottom-nav/chat.png',
+                      width: 24.0, height: 24.0),
+                  const Text('Chat'),
+                ]),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Column(children: [
+                  Image.asset('assets/icons/bottom-nav/user-profile.png',
+                      width: 24.0, height: 24.0),
+                  const Text('Profile'),
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
